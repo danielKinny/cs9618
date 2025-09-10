f4c0e817c5857573f54a0262717b555271d5b545
daniel kinny <danielkinny0214@gmail.com>
2025-09-10 12:02:13 +0400

    aaa

--- tuesday9sep/recursion.py
+++ tuesday9sep/recursion.py
@@ -11,4 +11,4 @@ def fib(n):
         return 1
     return fib(n-1)+fib(n-2)
 
-print(fib(1))
+print(fib(4))
---
+++ wednesday10sep/tracetable.py
@@ -0,0 +1,19 @@
+def factorial(n):
+
+    print("----------------------------")
+
+    print("recursive call number: ",n)
+    print("n==0: ",(n==0))
+
+    if n==0:
+        return 1
+    else:
+        print(n," * factorial(",n-1,")")
+
+    result = n*factorial(n-1)
+    print()
+    print("this part is : ", result)
+    return result
+
+print()
+print("The result of the factorial is: ",factorial(6))
