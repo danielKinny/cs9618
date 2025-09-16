1f09591c118d7ee6492bcc4c9bb43f02bc3625e7
daniel kinny <danielkinny0214@gmail.com>
2025-09-16 13:54:43 +0400

    aa

--- tuesday16sep/excephandle.py
+++ tuesday16sep/excephandle.py
@@ -4,3 +4,14 @@ except:
     print("there was a runtime error")
 finally:
     print("This prints at the end")
+
+while True:
+
+    string = input("Enter an integer: ")
+    try:
+        result = int(string)
+        break
+    except:
+        print("The thing thats been entered is not an integer")
+    finally:
+        print('This try-catch block has finished')
