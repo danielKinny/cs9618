fff0cf2e62109c6832b690a5d4b708396844e42b
daniel kinny <danielkinny0214@gmail.com>
2025-10-20 14:31:23 +0400

    Update task.py

--- monday20oct/task.py
+++ monday20oct/task.py
@@ -1,25 +1,30 @@
-arr = [1,2,3,4,5,6,7,8,9,10]
+arr = [7,12,19,23,27,33,37,41,56,59,60,62,71,75,80,84,88,92,99]
 
 found = False
 l = 0
 r = len(arr)-1
-target = 1
+target = 34
 c = 1
 
-while r>l and not found:
+while r>=l and not found:
     print(f"iteration number {c}")
     mid = (r+l)//2
     
     if arr[mid] == target:
         found = True
-        print("Number has been found")
     elif arr[mid] > target:
         r = mid-1
+        
     else:
         l = mid+1
         
-    r-=1
-    l+=1
     c+=1
+
+print()
+
+if found:
+    print("Number has been found")
+else:
+    print("This number is not within the list")
     
-print(f"iteration number {c}")
+print(f"total iterations {c}")
